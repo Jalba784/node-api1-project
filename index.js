6df68c924fa1f30685c78028beb901ce8927e95a
josepaniagua <jpanjr@gmail.com>
2020-04-12 00:15:11 -0700

    get all users

--- index.js
+++ index.js
@@ -9,28 +9,32 @@ server.use(express.json());
 
 // Root Route
 server.get("/", (req, res) => {
-    console.log("Root has been activated")
-    res.send("Big Bang Theory Characters")
+  console.log("Root has been activated");
+  res.send("Big Bang Theory Characters");
 });
 // Create User
 server.post("/users", (req, res) => {
-    if (!req.body.name || !req.body.bio) {
-        return res.status(400).json({
-            errorMessage: "Please provide name and bio for the user."
-        })
-    }
-    const newUser = db.createUser({
-        name: req.body.name,
-        bio: req.body.bio
-    })
-    res.status(201).json(newUser)
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+  }
+  const newUser = db.createUser({
+    name: req.body.name,
+    bio: req.body.bio
+  });
+  res.status(201).json(newUser);
 });
 // All Users
+server.get("/users", (req, res) => {
+  const users = db.getUsers();
+  res.json(users);
+});
 // User by id
 // Deletes specific user
 // Updates specific user
 
 // Server Listening
 server.listen(3222, () => {
-    console.log("Server started at port 3222")
+  console.log("Server started at port 3222");
 });
