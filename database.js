e89307646eaab7a702993efe155a1000c2cd3407
josepaniagua <jpanjr@gmail.com>
2020-04-12 00:10:57 -0700

    create user

--- database.js
+++ database.js
@@ -1,7 +1,26 @@
+const shortid = require("shortid");
+
 let users = [
-  { id: "1", name: "Jane Doe" },
-  { id: "2", name: "John Doe" },
-  { id: "3", name: "Jack Doe" }
+  {
+    id: `${shortid.generate()}`,
+    name: "Leonard",
+    bio: "An experimental physicist at Caltech in Pasadena. Although Leonard has many geeky interests and hobbies, he was the most willing of the guys to try and socialize with other people. Leonard's interest in Penny brought her into the guys social group and helped bring Leonard, and indeed all of the guys, out of their shells. Leonard and Penny were married 2015 and, after a year of splitting time between apartments 4A and 4B, now live alone in 4A."
+  },
+  {
+    id: `${shortid.generate()}`,
+    name: "Sheldon",
+    bio: "Theoretical physicist at Caltech who is married to neurobiologist Amy Farrah Fowler, with whom he now lives in Apartment 4B after years of sharing an apartment with his best friend and colleague, Leonard Hofstadter."
+  },
+  {
+    id: `${shortid.generate()}`,
+    name: "Penny",
+    bio: "A pharmaceutical sales rep for Zangen Pharmaceuticals, who formerly worked as a waitress at the Cheesecake Factory. Originally from Omaha, Nebraska, Penny moved to California with dreams of becoming a Hollywood actress. In 2007, she moved into the apartment across the hall from Sheldon and Leonard. In 2015, she and Leonard married and now live together in Apartment 4A."
+  },
+  {
+    id: `${shortid.generate()}`,
+    name: "Amy",
+    bio: "Dr. Amy Farrah Fowler, Ph.D. is a neurobiologist from Glendale, California, who is married to Dr. Sheldon Cooper. Amy met Sheldon Cooper after Howard and Raj created a fake dating profile for him, and Amy's profile was matched to his."
+  }
 ];
 
 function getUsers() {
--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const db = require("./database");
 
-// Server instance
+// Server Instance
 const server = express();
 
 // Middleware
 server.use(express.json());
 
+// Root Route
+server.get("/", (req, res) => {
+    console.log("Root has been activated")
+    res.send("Big Bang Theory Characters")
+});
+// Create User
+server.post("/users", (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        return res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+        })
+    }
+    const newUser = db.createUser({
+        name: req.body.name,
+        bio: req.body.bio
+    })
+    res.status(201).json(newUser)
+});
+// All Users
+// User by id
+// Deletes specific user
+// Updates specific user
+
+// Server Listening
+server.listen(3222, () => {
+    console.log("Server started at port 3222")
+});
